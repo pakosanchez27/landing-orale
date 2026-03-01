f1e5e3d80ce3754eabdf26a4befdad5397a450e7
Francisco Javier Sanchez tirado <javier.sanchezjps27@gmail.com>
2026-03-01 10:05:51 -0600

    se agrega seccion de demos

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,17 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: '/public/build/',
+    base: '/build/', // ⚠️ importante
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/home.css', 'resources/css/nosotros.css', 'resources/js/app.js', 'resources/css/normalize.css', 'resources/css/demos.css'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/home.css',
+                'resources/css/nosotros.css',
+                'resources/css/normalize.css',
+                'resources/css/demos.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
